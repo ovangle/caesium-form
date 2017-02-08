27614dd74c31de39bc05d5146188877058a0ff98
Thomas Stephenson <ovangle@gmail.com>
2017-02-08 19:21:08 +1100

    Build styles before rollup

--- gulpfile.js
+++ gulpfile.js
@@ -14,18 +14,7 @@ var ts = require('gulp-typescript');
 
 var tsProject = ts.createProject('./tsconfig.json');
 
-var bundles = [
-  'bootstrap',
-  'date',
-  'email',
-  'enum',
-  'icon',
-  'phone',
-  'spinner',
-  'toggle'
-];
-
-gulp.task('clean:spec', ['build'], function () {
+gulp.task('clean:spec', ['build:scripts'], function () {
   return series([
     del('./lib/**/*.spec.js'),
     del('./lib/**/*.spec.d.ts')
@@ -39,63 +28,79 @@ gulp.task('clean', function () {
   ])
 });
 
-gulp.task('build', function () {
+gulp.task('build:styles', function() {
+  return gulp.src('src/**/*.scss')
+    .pipe(sass({
+      includePaths: ['node_modules']
+    }))
+    .pipe(gulp.dest('lib'));
+});
+
+gulp.task('build:scripts', ['build:styles'], function () {
+
   return gulp.src('src/**/*.ts')
     .pipe(inline_templates({
       useRelativePaths: true,
       removeModuleId: true,
-
-      processStyles: sass({
-        includePaths: ['node_modules'],
-        omitSourceMapUrl: true
-      })
+      styleProcessor: function (filePath, ext, file, cb) {
+        relPath = path.relative(process.cwd(), filePath)
+          .replace('src', 'lib')
+          .replace('.scss', '.css');
+        let content = fs.readFileSync(relPath);
+        cb(null, content);
+      }
     }))
     .pipe(tsProject())
     .pipe(gulp.dest('./lib'));
 });
 
-gulp.task('build:rollup', ['build', 'clean:spec'], function () {
-  var globals = {
-    '@angular/core': 'ng.core',
-    '@angular/common': 'ng.common',
-    '@angular/forms': 'ng.forms',
 
-    'rxjs/Observable': 'Rx',
-    'rxjs/add/observable/fromEventPattern': 'Rx.Observable',
-    'rxjs/add/observable/defer': 'Rx.Observable.prototype',
-    'rxjs/add/operator/debounceTime': 'Rx.Observable.prototype',
-    'rxjs/add/operator/skipWhile': 'Rx.Observable.prototype',
-    'rxjs/add/operator/first': 'Rx.Observable.prototype',
-    'rxjs/add/operator/map': 'Rx.Observable.prototype',
-    'rxjs/add/operator/filter': 'Rx.Observable.prototype',
-    'rxjs/add/operator/toPromise': 'Rx.Observable.prototype',
+gulp.task(
+  'build:rollup',
+  ['build:styles', 'build:scripts', 'clean:spec'],
+  function () {
+    var globals = {
+      '@angular/core': 'ng.core',
+      '@angular/common': 'ng.common',
+      '@angular/forms': 'ng.forms',
 
-    'immutable': 'Immutable',
+      'rxjs/Observable': 'Rx',
+      'rxjs/add/observable/fromEventPattern': 'Rx.Observable',
+      'rxjs/add/observable/defer': 'Rx.Observable.prototype',
+      'rxjs/add/operator/debounceTime': 'Rx.Observable.prototype',
+      'rxjs/add/operator/skipWhile': 'Rx.Observable.prototype',
+      'rxjs/add/operator/first': 'Rx.Observable.prototype',
+      'rxjs/add/operator/map': 'Rx.Observable.prototype',
+      'rxjs/add/operator/filter': 'Rx.Observable.prototype',
+      'rxjs/add/operator/toPromise': 'Rx.Observable.prototype',
 
-    'moment': 'moment',
+      'immutable': 'Immutable',
 
-    'caesium-core/lang': 'cs.core.lang',
-    'caesium-core/converter': 'cs.core.converter',
-    'caesium-core/codec': 'cs.core.codec',
-    'caesium-core/exception': 'cs.core.exception',
-    'caesium-model': 'cs.model'
-  };
+      'moment': 'moment',
 
-  var rollupOptions = {
-    context: 'this',
-    external: Object.keys(globals),
-  };
+      'caesium-core/lang': 'cs.core.lang',
+      'caesium-core/converter': 'cs.core.converter',
+      'caesium-core/codec': 'cs.core.codec',
+      'caesium-core/exception': 'cs.core.exception',
+      'caesium-model': 'cs.model'
+    };
 
-  const rollupGenerateOptions = {
-    moduleId: '',
-    moduleName: 'cs.forms',
-    format: 'umd',
-    globals,
-    dest: 'caesium-form.umd.js'
-  };
+    var rollupOptions = {
+      context: 'this',
+      external: Object.keys(globals),
+    };
 
-  return gulp.src(path.join('lib', 'module.js'))
-    .pipe(rollup(rollupOptions, rollupGenerateOptions))
-    .pipe(gulp.dest('bundles'));
-});
+    const rollupGenerateOptions = {
+      moduleId: '',
+      moduleName: 'cs.forms',
+      format: 'umd',
+      globals,
+      dest: 'caesium-form.umd.js'
+    };
+
+    return gulp.src(path.join('lib', 'module.js'))
+      .pipe(rollup(rollupOptions, rollupGenerateOptions))
+      .pipe(gulp.dest('bundles'));
+  }
+);
 
